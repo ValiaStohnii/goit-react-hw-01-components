2ddda7b4bf8a1de0c1c60632906e38df0ce6e9dd
ValiaStohnii <valia19961018@gmail.com>
2021-12-19 20:23:56 +0200

    Updates

--- static/js/main.a0cec84d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var c=n(1),o=n.n(c),s=n(3),r=n.n(s),i=(n(8),n(9),n(0));var a=function(){return Object(i.jsx)("div",{className:"App",children:"test"})};r.a.render(Object(i.jsx)(o.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.a0cec84d.chunk.js.map
